dad3f944ea83f270077e288a86763e39b22805fd
Tangent-T01 <157572902+Tangent-T01@users.noreply.github.com>
2024-01-23 23:09:43 -0800

    Initial Commit

---
+++ frameSorter.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+from tkinter import Tk, PhotoImage, Canvas
+
+loopPoint = 0
+
+folder_path = 'FramesOriginal'
+image_paths = []
+
+for filename in os.listdir(folder_path):
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+        image_path = os.path.join(folder_path, filename)
+        image_paths.append(image_path)
+
+def move_file(key):
+    global loopPoint
+    if loopPoint < len(image_paths):
+        source_path = image_paths[loopPoint]
+        folder_path_1 = "Horizontal"
+        folder_path_2 = "Vertical"
+        if key == "1":
+            destination = folder_path_1
+        elif key == "2":
+            destination = folder_path_2
+        else:
+            return
+        shutil.move(source_path, destination)
+        loopPoint += 1
+        update_ui()
+
+def update_ui():
+    image_path = image_paths[loopPoint]
+    img = PhotoImage(file=image_path)
+    canvas.itemconfig(image_item, image=img)
+    canvas.image = img
+
+def on_key(event):
+    move_file(event.char)
+
+root = Tk()
+root.title("Image Mover")
+
+image_path = image_paths[loopPoint]
+img = PhotoImage(file=image_path)
+canvas = Canvas(root, width=img.width(), height=img.height())
+image_item = canvas.create_image(0, 0, anchor='nw', image=img)
+canvas.pack()
+
+root.bind("<Key>", on_key)
+
+
+root.mainloop()
